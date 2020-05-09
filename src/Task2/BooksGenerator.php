95db3f45177cff2ae9c9fae778c355f6a45209b0
ykw1n <proo1oomail@gmail.com>
2020-05-09 09:56:55 +0300

    added constructor to books and books generator

--- src/Task2/BooksGenerator.php
+++ src/Task2/BooksGenerator.php
@@ -6,8 +6,31 @@ namespace App\Task2;
 
 class BooksGenerator
 {
+    public int $minPagesNumber;
+    public array $libraryBooks;
+    public int $maxPrice;
+    public array $storeBook;
+
+    /**
+     * BooksGenerator constructor.
+     * @param $minPagesNumber
+     * @param $libraryBooks
+     * @param $maxPrice
+     * @param $storeBook
+     */
+    public function __construct($minPagesNumber, $libraryBooks, $maxPrice, $storeBook)
+    {
+        $this->minPagesNumber = $minPagesNumber;
+        $this->libraryBooks = $libraryBooks;
+        $this->maxPrice = $maxPrice;
+        $this->storeBook = $storeBook;
+    }
+
+
     public function generate(): \Generator
     {
-        //@todo
+
+
+
     }
 }
